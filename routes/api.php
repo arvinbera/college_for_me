b24bdda862bd82a4c84e526866f8b56141cec3f3
arvin-git123 <arvin.bera@gmail.com>
2025-01-21 15:51:35 +0530

    sab i tultul

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CollegeAdmin\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('collegeadmin/login', [AuthenticationController::class, 'college_admin_login']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
