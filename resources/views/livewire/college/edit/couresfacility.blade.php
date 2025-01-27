ba2eaf0e3d243671d91f96036f4bf80b0957d9ed
arvin-git123 <arvin.bera@gmail.com>
2025-01-27 10:40:40 +0530

    27-01-25

--- resources/views/livewire/college/edit/couresfacility.blade.php
+++ resources/views/livewire/college/edit/couresfacility.blade.php
@@ -1,28 +1,33 @@
-<table class="table card-table table-vcenter text-nowrap datatable">
-    <thead>
-        <tr>
-            <th class="w-1">Sl</th>
-            <th>Course name</th>
-            <th>Department name</th>
-            <th>Eligibility</th>
-            <th>Fees</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody class="table-tbody">
-
-        @foreach ($course_eligibilities_fees as $course)
+<div>
+    <table class="table card-table table-vcenter text-nowrap datatable">
+        <thead>
             <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $course->course->course_name }}</td>
-                <td>{{ $course->department->department_name }}</td>
-                <td>{{ $course->eligibility }}</td>
-                <td>{{ $course?->fees?->fees }}</td>
-                <td><a href="{{route('college.course.fees.eligibility.edit', [$course->id,$course->college_id])}}"
-                        class="btn btn-sm btn-outline-primary">Edit</a></td>
+                <th class="w-1">Sl</th>
+                <th>Course name</th>
+                <th>Department name</th>
+                <th>Eligibility</th>
+                <th>Fees</th>
+                <th>Action</th>
             </tr>
-        @endforeach
-
-    </tbody>
-
-</table>
+        </thead>
+        <tbody class="table-tbody">
+    
+            @foreach ($course_eligibilities_fees as $course)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $course->course->course_name }}</td>
+                    <td>{{ $course->department->department_name }}</td>
+                    <td>{{ $course->eligibility }}</td>
+                    <td>{{ $course?->fees?->fees }}</td>
+                    <td><a href="{{ route('college.course.fees.eligibility.edit', [$course->id, $course->college_id]) }}"
+                            class="btn btn-sm btn-outline-primary">Edit</a></td>
+                </tr>
+            @endforeach
+    
+        </tbody>
+    
+    </table>
+    <div class="d-flex justify-items-center mt-4">
+        <div class=" mx-auto"><a href="" class="btn btn-sm btn-outline-primary">Add Course</a></div>
+    </div>
+</div>
