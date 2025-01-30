aec02767d8863d70ba0902c40967bf5a5e898f85
arnab-tech-oss <arnabrofficial@gmail.com>
2025-01-30 16:44:55 +0530

    api done

--- app/Http/Controllers/apicontroller/CourseController.php
+++ app/Http/Controllers/apicontroller/CourseController.php
@@ -3,19 +3,19 @@
 namespace App\Http\Controllers\apicontroller;
 
 use App\Http\Controllers\Controller;
-use App\Models\CollegeFaculty;
+use App\Models\Course;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
-class FacultyController extends Controller
+class CourseController extends Controller
 {
     public function index()
     {
-        $college_faculty = CollegeFaculty::all();
-        if ($college_faculty->count() > 0){
+        $college_course = Course::all();
+        if ($college_course->count() > 0){
             $data = [
                 'success'=> true,
-                'college_faculty'=> $college_faculty,
+                'college_faculty'=> $college_course,
                 'statusCode' => 200,
                 'message'=> 'Records fetch successfully',
             ];
@@ -34,22 +34,22 @@ class FacultyController extends Controller
 
     public function edit($id)
     {
-        $college_faculty = CollegeFaculty::find($id);
-        
-        if ($college_faculty){
+        $college_course = Course::find($id);
+
+        if ($college_course) {
             return response()->json([
                 'success'=> true,
-                'Data'=> $college_faculty,
-                'statusCode'=> 200,
-                'message'=> "Records fetch successfully",
+                'data'=> $college_course,
+                'statusCode' => 200,
+                'message'=> 'Records fetch successfully',
             ], 200);
         } else {
             return response()->json([
                 'success' => false,
                 'error'=>[
-                  'message'=> "No such records found !",
+                  'message'=> 'NO such records found !',
                 ],
-                 'statusCode' => 404,
+                'statusCode' => 404,
             ], 404);
         }
     }
@@ -57,36 +57,33 @@ class FacultyController extends Controller
     public function update(Request $request, int $id)
     {
         $validator = Validator::make($request->all(),[
-            'college_id' => 'required',
-            'department_id' => 'required',
-            'faculty_name'=> 'required|string|max:191',
-            'faculty_designation' => 'required|string|max:191'
+            'course_name' => 'required',
+            'course_duration' => 'required',
         ]);
-        if ($validator->fails()) {
+        if ($validator->fails()){
             return response()->json([
-                'success' => false,
+                'success'=> false,
                 'error'=> $validator->messages(),
             ], 404);
         } else {
-            $college_faculty = CollegeFaculty::find($id);
+            $college_course = Course::find($id);
 
-            if ($college_faculty) {
-                $college_faculty->update([
-                    'college_id' => $request->college_id,
-                    'department_id' => $request->department_id,
-                    'faculty_name' => $request->faculty_name,
-                    'faculty_designation' => $request->faculty_designation,
+            if($college_course){
+                $college_course->update([
+                    'course_name'=> $request->course_name,
+                    'course_slug'=>$request->course_name,
+                    'course_duration'=> $request->course_duration,
                 ]);
                 return response()->json([
-                     'success'=> true,
+                    'success'=> true,
                     'message'=> 'Data updated successfully !',
                     'statusCode' => 202
                 ], 202);
             } else {
                 return response()->json([
-                     'success'=> false,
-                    'error'=> [
-                        'message'=> 'No such records founds',
+                    'success'=> false,
+                    'error'=>[
+                      'message'=> 'No such record found'
                     ],
                     'statusCode' => 404,
                 ], 404);
